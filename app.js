26c730d6f890f99ed63043728c17e2ab85f363c7
alexandr.v@affinidi.com <alexandr.v@affinidi.com>
2021-09-20 14:18:49 +0300

    commit 20.09

--- app.js
+++ app.js
@@ -1,15 +1,70 @@
 require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const routes = require('./src/routes')
+const fs = require('fs')
+const {createHash} = require('crypto')
 
-app.use(bodyParser.json());
-app.use(express.json())
-app.use(routes)
+const Koa = require('koa')
+const Cors = require('@koa/cors')
+const Router = require('@koa/router')
+const Body = require('koa-body')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args))
+const jwt = require('jsonwebtoken')
+const FormData = require('form-data')
 
-const port = process.env.PORT || 4000
+const {
+  PORT = 3000,
+  LICENSE,
+  PRIVATE_KEY_PATH,
+  SECRET_PASSPHRASE,
+  COMMON_CHECK_ENDPOINT,
+} = process.env
 
-app.listen(port, () => {
-  console.log(`Server started on ${port}`)
-});
+const privateKey = fs.readFileSync(PRIVATE_KEY_PATH)
+
+const hashFile = file => createHash('sha256').update(file).digest('hex')
+const signJwt = payload => jwt.sign(payload, {
+  key       : privateKey,
+  passphrase: SECRET_PASSPHRASE,
+}, { algorithm: 'RS512' })
+
+const router = new Router().post('/api/v1/verify', async context => {
+  const { request: { files: { file } } } = context
+  const requestId = context.get('Request-ID')
+  const fileBuffer = fs.readFileSync(file.path)
+  const fileHash = await hashFile(fileBuffer)
+  const authorization = signJwt(fileHash)
+  const formData = new FormData()
+
+  console.log('===>requestId', requestId);
+  console.log('===>fileBuffer', fileBuffer);
+  console.log('===>fileHash', fileHash);
+  console.log('===>authorization', authorization);
+
+  formData.append('file', fileBuffer, {
+    name    : 'file',
+    filename: 'encryptedFile',
+  })
+
+  console.log('===>formData', formData);
+
+  const response = await fetch(COMMON_CHECK_ENDPOINT, {
+    method : 'POST',
+    body   : formData,
+    headers: {
+      'Api-Key'      : LICENSE,
+      'Authorization': authorization,
+      'Request-ID'   : requestId,
+      ...formData.getHeaders(),
+    },
+    timeout: 15000,
+  })
+
+  context.body = await response.json()
+  console.log('===>  context.body',   context.body);
+})
+
+new Koa()
+  .use(Cors())
+  .use(Body({ multipart: true }))
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(PORT, () => console.log('🥐 App running', PORT ))
